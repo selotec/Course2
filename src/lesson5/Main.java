bf1b4d34afbcd8b7111d21fb0bbd5d280b79f7e5
selotec <selotec@gmail.com>
2019-08-11 19:45:29 +0700

    Lesson5

---
+++ src/lesson5/Main.java
@@ -0,0 +1,11 @@
+package lesson5;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Benchmark bm1 = new Benchmark();
+        bm1.sequentialCalc();
+        bm1.parallelCalc();
+    }
+}
